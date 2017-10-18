f5caa5b147aa0e5fd0efd2754dffad7d4ed59d1c
Sven ten Haaf <sventenhaaf@hotmail.com>
2017-10-18 17:41:57 +0200

    mobile click and focus ok

--- js/clicks.js
+++ js/clicks.js
@@ -12,6 +12,24 @@ if (!mobilecheck()) {
   })
 }
 
+if (mobilecheck()) {
+  [20, 21, 22, 23, 24, 25, 26, 27, 28, 29].forEach(n => {
+    var el = document.getElementById("delay" + n);
+    el.addEventListener("click", e => {
+      var overlay = el.querySelector(".overlay");
+      if (overlay.className.includes(" clicked")) {
+        overlay.className = overlay.className.split(" clicked")[0];
+        overlay.style.opacity = 0;
+        return;
+      } else {
+        e.preventDefault();
+        overlay.className = overlay.className + " clicked";
+        overlay.style.opacity = 0.8;
+      }
+    });
+  });
+}
+
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   21, 22, 23, 24, 25, 26, 27, 28, 29].forEach(n => {
     var el = document.getElementById("delay" + n);
